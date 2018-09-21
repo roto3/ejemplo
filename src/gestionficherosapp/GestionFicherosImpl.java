e21fa88620a1d42c8d4e367d5b7510fb0b7b4125
roto3 <cludefanslynxoficial@gmail.com>
2018-09-21 15:21:52 +0200

    comit prueba

---
+++ src/gestionficherosapp/GestionFicherosImpl.java
@@ -0,0 +1,391 @@
+package gestionficherosapp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import gestionficheros.FormatoVistas;
+import gestionficheros.GestionFicheros;
+import gestionficheros.GestionFicherosException;
+import gestionficheros.TipoOrden;
+
+public class GestionFicherosImpl implements GestionFicheros {
+	private File carpetaDeTrabajo = null;
+	private Object[][] contenido;
+	private int filas = 0;
+	private int columnas = 3;
+	private FormatoVistas formatoVistas = FormatoVistas.NOMBRES;
+	private TipoOrden ordenado = TipoOrden.DESORDENADO;
+
+	public GestionFicherosImpl() {
+		carpetaDeTrabajo = File.listRoots()[0];
+		actualiza();
+	}
+
+	private void actualiza() {
+
+		String[] ficheros = carpetaDeTrabajo.list(); // obtener los nombres
+		// calcular el número de filas necesario
+		filas = ficheros.length / columnas;
+		if (filas * columnas < ficheros.length) {
+			filas++; // si hay resto necesitamos una fila más
+		}
+
+		// dimensionar la matriz contenido según los resultados
+
+		contenido = new String[filas][columnas];
+		// Rellenar contenido con los nombres obtenidos
+		for (int i = 0; i < columnas; i++) {
+			for (int j = 0; j < filas; j++) {
+				int ind = j * columnas + i;
+				if (ind < ficheros.length) {
+					contenido[j][i] = ficheros[ind];
+				} else {
+					contenido[j][i] = "";
+				}
+			}
+		}
+	}
+
+	@Override
+	public void arriba() {
+
+		System.out.println("holaaa");
+		if (carpetaDeTrabajo.getParentFile() != null) {
+			carpetaDeTrabajo = carpetaDeTrabajo.getParentFile();
+			actualiza();
+		}
+
+	}
+
+	@Override
+	public void creaCarpeta(String arg0) throws GestionFicherosException {
+		File file = new File(carpetaDeTrabajo,arg0);
+		carpetaDeTrabajo.exists();
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//que se pueda escribir -> lanzará una excepción
+		//que no exista -> lanzará una excepción
+		//crear la carpeta -> lanzará una excepción
+		actualiza();
+	}
+
+	@Override
+	public void creaFichero(String arg0) throws GestionFicherosException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void elimina(String arg0) throws GestionFicherosException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void entraA(String arg0) throws GestionFicherosException {
+		File file = new File(carpetaDeTrabajo, arg0);
+		// se controla que el nombre corresponda a una carpeta existente
+		if (!file.isDirectory()) {
+			throw new GestionFicherosException("Error. Se ha encontrado "
+					+ file.getAbsolutePath()
+					+ " pero se esperaba un directorio");
+		}
+		// se controla que se tengan permisos para leer carpeta
+		if (!file.canRead()) {
+			throw new GestionFicherosException("Alerta. No se puede acceder a "
+					+ file.getAbsolutePath() + ". No hay permiso");
+		}
+		// nueva asignación de la carpeta de trabajo
+		carpetaDeTrabajo = file;
+		// se requiere actualizar contenido
+		actualiza();
+
+	}
+
+	@Override
+	public int getColumnas() {
+		return columnas;
+	}
+
+	@Override
+	public Object[][] getContenido() {
+		return contenido;
+	}
+
+	@Override
+	public String getDireccionCarpeta() {
+		return carpetaDeTrabajo.getAbsolutePath();
+	}
+
+	@Override
+	public String getEspacioDisponibleCarpetaTrabajo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEspacioTotalCarpetaTrabajo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getFilas() {
+		return filas;
+	}
+
+	@Override
+	public FormatoVistas getFormatoContenido() {
+		return formatoVistas;
+	}
+
+	@Override
+	public String getInformacion(String arg0) throws GestionFicherosException {
+		
+		StringBuilder strBuilder=new StringBuilder();
+		File file = new File(carpetaDeTrabajo,arg0);
+		
+		//Controlar que existe. Si no, se lanzará una excepción
+		//Controlar que haya permisos de lectura. Si no, se lanzará una excepción
+		if (file.exists()==false) {
+			throw new GestionFicherosException("Alerta. El archivo o directorio "
+					+ file.getName() + " no existe o es inalcanzable.");
+		}else if((file.canExecute()==false)||(file.canRead()==false)||(file.canWrite()==false)){
+			throw new GestionFicherosException("Alerta. No posee los permisos suficientes");
+		}
+		//Título
+		strBuilder.append("INFORMACIÓN DEL SISTEMA");
+		strBuilder.append("\n\n");
+		
+		//Nombre
+		strBuilder.append("Nombre: ");
+		strBuilder.append(arg0);
+		strBuilder.append("\n");
+		
+		
+		strBuilder.append("Tipo: ");
+		String Tipo ="";
+		if (file.isFile()==true){
+			Tipo="Archivo";
+			strBuilder.append(Tipo);
+			strBuilder.append("\n");
+		}else{
+			Tipo="Directorio";
+			strBuilder.append(Tipo);
+			strBuilder.append("\n");
+			String archivos[] = file.list();
+			strBuilder.append("Archivos contenidos: ");
+			strBuilder.append(archivos.length);
+			strBuilder.append("\n");
+			
+			strBuilder.append("Espacio libre: ");
+			strBuilder.append(file.getFreeSpace());
+			strBuilder.append("\n");
+			strBuilder.append("Espacio disponible: : ");
+			strBuilder.append(file.getUsableSpace());
+			strBuilder.append("\n");
+			strBuilder.append("Espacio total: ");
+			strBuilder.append(file.getTotalSpace());
+			strBuilder.append("\n");
+		};
+		
+
+		strBuilder.append("Ubicación: ");
+		strBuilder.append(file.getAbsolutePath());
+		strBuilder.append("\n");
+		
+		
+		strBuilder.append("Fecha (ult. mod.): ");
+		new SimpleDateFormat("dd-MM-yyyy HH-mm-ss").format(
+			    new Date(file.lastModified()) 
+			);
+		strBuilder.append(file.lastModified());
+		strBuilder.append("\n");
+		
+		
+		strBuilder.append("Oculto: ");
+		String oculto ="";
+		if (file.isHidden()==true){
+			oculto="Sí";
+		}else{
+			oculto="No";
+		}
+		strBuilder.append(oculto);
+		strBuilder.append("\n");
+		
+		
+		strBuilder.append("Tamaño: ");
+		strBuilder.append(file.getTotalSpace());
+		strBuilder.append(" bytes");
+		strBuilder.append("\n");
+		//Tipo: fichero o directorio
+		
+		//Ubicación
+		
+		//Fecha de última modificación
+		
+		//Si es un fichero oculto o no
+		
+		//Si es un fichero: Tamaño en bytes
+		
+		//Si es directorio: Número de elementos que contiene, 
+		//Si es directorio: Espacio libre, espacio disponible, espacio total (bytes)
+				
+		return strBuilder.toString();
+	}
+
+	@Override
+	public boolean getMostrarOcultos() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getNombreCarpeta() {
+		return carpetaDeTrabajo.getName();
+	}
+
+	@Override
+	public TipoOrden getOrdenado() {
+		return ordenado;
+	}
+
+	@Override
+	public String[] getTituloColumnas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getUltimaModificacion(String arg0)
+			throws GestionFicherosException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String nomRaiz(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numRaices() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void renombra(String arg0, String arg1)
+			throws GestionFicherosException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean sePuedeEjecutar(String arg0) throws GestionFicherosException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean sePuedeEscribir(String arg0) throws GestionFicherosException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean sePuedeLeer(String arg0) throws GestionFicherosException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setColumnas(int arg0) {
+		columnas = arg0;
+
+	}
+
+	@Override
+	public void setDirCarpeta(String arg0) throws GestionFicherosException {
+		File file = new File(arg0);
+
+		// se controla que la dirección exista y sea directorio
+		if (!file.isDirectory()) {
+			throw new GestionFicherosException("Error. Se esperaba "
+					+ "un directorio, pero " + file.getAbsolutePath()
+					+ " no es un directorio.");
+		}
+
+		// se controla que haya permisos para leer carpeta
+		if (!file.canRead()) {
+			throw new GestionFicherosException(
+					"Alerta. No se puede acceder a  " + file.getAbsolutePath()
+							+ ". No hay permisos");
+		}
+
+		// actualizar la carpeta de trabajo
+		carpetaDeTrabajo = file;
+
+		// actualizar el contenido
+		actualiza();
+
+	}
+
+	@Override
+	public void setFormatoContenido(FormatoVistas arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setMostrarOcultos(boolean arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setOrdenado(TipoOrden arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSePuedeEjecutar(String arg0, boolean arg1)
+			throws GestionFicherosException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSePuedeEscribir(String arg0, boolean arg1)
+			throws GestionFicherosException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSePuedeLeer(String arg0, boolean arg1)
+			throws GestionFicherosException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setUltimaModificacion(String arg0, long arg1)
+			throws GestionFicherosException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
